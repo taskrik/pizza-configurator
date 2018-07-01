e6de0040c139b52f860b464d841528437b6cabf4
tasos <tasos.krikonis@gmail.com>
2018-07-01 16:01:30 +0200

    almost done

--- src/reducers/pizza.js
+++ src/reducers/pizza.js
@@ -11,85 +11,73 @@ import { pizzabases, sauses, toppings } from '../shared/data'
 const initialState = {
   base: {
     id: '',
-    price: ''
+    price: 0
   },
   sauses: {
     id: '',
-    price: ''
+    price: 0
   },
   toppings: [],
   delivery: false,
   totalPrice: 0
 }
 
+const totalPrice = (state) => {
+  var totalPrice = 0;
+  totalPrice = state.base.price + state.sauses.price;
+  if (state.toppings.length > 0) {
+    state.toppings.map((topping) => {
+      totalPrice = totalPrice + topping.price;
+    });
+  }
+
+  if (state.delivery === true) totalPrice = totalPrice + totalPrice * 0.1;
+  return +(Math.round(totalPrice + "e+2") + "e-2")
+}
+
 export default function (state = initialState, action) {
   switch (action.type) {
     case NEW_BASE:
-      var price = state.totalPrice + pizzabases[action.payload.id - 1].price;
-      if (state.base.price !== '') {
-        price = state.totalPrice - state.base.price + pizzabases[action.payload.id - 1].price;
+      state.base = {
+        id: pizzabases[action.payload.id - 1].id,
+        price: pizzabases[action.payload.id - 1].price
       }
-      return {
-        ...state,
-        base: {
-          id: pizzabases[action.payload.id - 1].id,
-          price: pizzabases[action.payload.id - 1].price
-        },
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.totalPrice = totalPrice(state);
+      return { ...state };
     case NEW_SAUSE:
-      var price = state.totalPrice + sauses[action.payload.id - 1].price;
-      if (state.sauses.price !== '') {
-        price = parseFloat(state.totalPrice - state.sauses.price + sauses[action.payload.id - 1].price);
+      state.sauses = {
+        id: sauses[action.payload.id - 1].id,
+        price: sauses[action.payload.id - 1].price
       }
-      return {
-        ...state,
-        sauses: {
-          id: sauses[action.payload.id - 1].id,
-          price: sauses[action.payload.id - 1].price
-        },
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.totalPrice = totalPrice(state);
+      return { ...state };
     case NEW_TOPPING:
-      var price = state.totalPrice + toppings[action.payload.id - 1].price;
-      return {
-        ...state,
-        toppings: [...state.toppings, {
-          id: toppings[action.payload.id - 1].id,
-          price: toppings[action.payload.id - 1].price
-        }],
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.toppings = [...state.toppings, {
+        id: toppings[action.payload.id - 1].id,
+        price: toppings[action.payload.id - 1].price
+      }]
+      state.totalPrice = totalPrice(state);
+      return { ...state };
     case REMOVE_TOPPING:
       var amount = state.toppings.length;
       if (amount === 1) {
         state.toppings = [];
       } else {
         var toppingsArray = state.toppings;
-        for (var i = 0; i < amount; i++) {
-          if (toppingsArray[i].id === action.payload.id - 1) {
-            state.toppings.splice(i, 1);
-            break;
-          }
-        }
+        toppingsArray.map((topping, index) => {
+          if(topping.id === action.payload.id - 1) state.toppings.splice(index, 1);
+        });
       }
-      var price = state.totalPrice - toppings[action.payload.id - 1].price;
-      return {
-        ...state,
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.totalPrice = totalPrice(state);
+      return { ...state }
     case DRONE_DELIVERY:
-      var price = state.totalPrice + state.totalPrice * 0.10;
-      return {
-        ...state,
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.delivery = action.payload.select;
+      state.totalPrice = totalPrice(state);
+      return { ...state }
     case NODRONE_DELIVERY:
-      var price = state.totalPrice * 100 / 110;
-      return {
-        ...state,
-        totalPrice: +(Math.round(price + "e+2") + "e-2")
-      }
+      state.delivery = action.payload.select;
+      state.totalPrice = totalPrice(state);
+      return { ...state }
     default:
       return state
   }
